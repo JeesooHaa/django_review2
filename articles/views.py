2d3105818ae0a0f6558bebf52e7b4e9bd3b40ca0
JeesooHaa <haajeesoo@gmail.com>
2019-10-31 17:54:05 +0900

    Add JavaScript

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST, require_GET
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 from IPython import embed
@@ -133,9 +133,16 @@ def like(request, article_pk):
     if article.liked_users.filter(pk=user.pk).exists():
     # if user in article.liked_users.all():
         user.liked_articles.remove(article)
+        liked = False 
     else:
         user.liked_articles.add(article)
-    return redirect('articles:detail', article_pk)
+        liked = True
+
+    context = {
+        'liked': liked, 
+        'count': article.liked_users.count()
+    }
+    return JsonResponse(context)
 
 
 @login_required
